9524ed5dbb5c0632534b59af5e49829083833fb9
aman kaushik <vinukaushik29may@gmail.com>
2021-10-06 19:01:31 +0530

    second commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
+//require('dotenv').config()
 const express = require('express')
 const app = express()
-const Port = process.env.PORT || 4444                    //OR condition if Ist part not work thn second
+const PORT = process.env.PORT || 4444                    //OR condition if Ist part not work thn second
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(Port , ()=>
+app.listen(PORT , ()=>
 {
-    console.log(`http://localhost:${Port}`)
+    console.log(`http://localhost:${PORT}`)
 })
